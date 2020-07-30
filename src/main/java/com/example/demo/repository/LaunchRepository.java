4ed8af6eb4908a0850889544eb23a9248636722f
douglasmoreira <douglas.cosmo7@gmail.com>
2020-07-30 17:45:25 -0300

    correct sql in Launch repository

--- src/main/java/com/example/demo/repository/LaunchRepository.java
+++ src/main/java/com/example/demo/repository/LaunchRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Launch;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,10 +13,10 @@ import java.util.List;
 
 
 @Transactional(readOnly = true)
-@NamedQueries({@NamedQuery(name = "LaunchRepository.findByEmployeeId", query = "SELECT launch FROM launch WHERE launch.employee.id = :employeeId")})
+@NamedQueries({@NamedQuery(name = "LaunchRepository.findByEmployeeId", query = "SELECT Launch FROM Launch WHERE Launch.employee.id = :employeeId")})
 public interface LaunchRepository extends JpaRepository<Launch, Long> {
 
     List<Launch> findByEmployeeId(@Param("employeeId") Long employeeId);
 
-    List<Launch> findByEmployeeId(@Param("employeeId") Long employeeId, Pageable pageable);
+    Page<Launch> findByEmployeeId(@Param("employeeId") Long employeeId, Pageable pageable);
 }
