dac225daef5655bfc36238e510a2f7d17703ff8d
douglasmoreira <douglas.cosmo7@gmail.com>
2020-07-30 17:53:20 -0300

    test if the class have problem

--- src/main/java/com/example/demo/repository/CompanyRepository.java
+++ src/main/java/com/example/demo/repository/CompanyRepository.java
@@ -1,13 +1,13 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-@Repository
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-
-    @Transactional(readOnly = true)
-    Company findByCnpj(String cnpj);
-}
+//package com.example.demo.repository;
+//
+//import com.example.demo.entity.Company;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@Repository
+//public interface CompanyRepository extends JpaRepository<Company, Long> {
+//
+//    @Transactional(readOnly = true)
+//    Company findByCnpj(String cnpj);
+//}
