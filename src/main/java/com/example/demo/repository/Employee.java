55485c5be3d2f0b77e6c85743ef4e698f0af2e0e
douglasmoreira <douglas.cosmo7@gmail.com>
2020-07-30 18:21:30 -0300

    test if the class have problem

--- src/main/java/com/example/demo/repository/Employee.java
+++ src/main/java/com/example/demo/repository/Employee.java
@@ -1,15 +1,15 @@
-//package com.example.demo.repository;
-//
-//import com.example.demo.entity.Employee;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@Transactional(readOnly = true)
-//interface EmployeeRepository extends JpaRepository<Employee, Long> {
-//
-//    Employee findByCpf(String cpf);
-//
-//    Employee findByEmail(String email);
-//
-//    Employee findByCpfOrEmail(String cpf, String email);
-//}
+package com.example.demo.repository;
+
+import com.example.demo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Employee findByCpf(String cpf);
+
+    Employee findByEmail(String email);
+
+    Employee findByCpfOrEmail(String cpf, String email);
+}
--- src/main/java/com/example/demo/repository/LaunchRepository.java
+++ src/main/java/com/example/demo/repository/LaunchRepository.java
@@ -1,22 +1,22 @@
-//package com.example.demo.repository;
-//
-//import com.example.demo.entity.Launch;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.repository.query.Param;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import javax.persistence.NamedQueries;
-//import javax.persistence.NamedQuery;
-//import java.util.List;
-//
-//
-//@Transactional(readOnly = true)
-//@NamedQueries({@NamedQuery(name = "LaunchRepository.findByEmployeeId", query = "SELECT Launch FROM Launch WHERE Launch.employee.id = :employeeId")})
-//public interface LaunchRepository extends JpaRepository<Launch, Long> {
-//
-//    List<Launch> findByEmployeeId(@Param("employeeId") Long employeeId);
-//
-//    Page<Launch> findByEmployeeId(@Param("employeeId") Long employeeId, Pageable pageable);
-//}
+package com.example.demo.repository;
+
+import com.example.demo.entity.Launch;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import java.util.List;
+
+
+@Transactional(readOnly = true)
+@NamedQueries({@NamedQuery(name = "LaunchRepository.findByEmployeeId", query = "SELECT Launch FROM Launch WHERE Launch.employee.id = :employeeId")})
+public interface LaunchRepository extends JpaRepository<Launch, Long> {
+
+    List<Launch> findByEmployeeId(@Param("employeeId") Long employeeId);
+
+    Page<Launch> findByEmployeeId(@Param("employeeId") Long employeeId, Pageable pageable);
+}
